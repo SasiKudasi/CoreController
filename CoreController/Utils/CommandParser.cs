d96dbf20f6b3694f0b6d4f906e98b22a95339cdb
SasaiKudasai <sashamittal1@gmail.com>
2026-01-08 15:06:10 +0300

    command name

--- CoreController/Utils/CommandParser.cs
+++ CoreController/Utils/CommandParser.cs
@@ -7,7 +7,7 @@ namespace CoreController.Utils
         public static string ParseCommand(string input) => input.ToLower() switch
         {
             var c when c.Contains("включи") && c.Contains("пк") => "wake_pc",
-            var c when c.Contains("дев") && c.Contains("мод") => "work_mode",
+            var c when c.Contains("рабочий") && c.Contains("режим") => "work_mode",
             var c when c.Contains("гейм") && c.Contains("мод") => "game_mode",
             var c when c.Contains("релакс") && c.Contains("мод") => "relax_mode",
             _ => "unknown"
